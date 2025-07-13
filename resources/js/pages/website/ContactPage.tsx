3e84bad0a11da877fbf12bcca88cc68097bd56e0
shikhar26shukla <shikhar@devloops.in>
2025-07-13 11:49:56 +0530

    Contact page dynamic

--- resources/js/pages/website/ContactPage.tsx
+++ resources/js/pages/website/ContactPage.tsx
@@ -1,16 +1,21 @@
 'use client';
 
+import { SiteSettings } from '@/types/site-settings';
 import { useTranslation } from 'react-i18next';
 import ContactCTA from './Contact';
 import Layout from './Layout';
 
-const ContactPage = () => {
+interface Props {
+    siteSettings: SiteSettings;
+}
+
+const ContactPage = ({ siteSettings }: Props) => {
     const { t } = useTranslation();
 
     return (
         <Layout title={`${t('contact_us')} | Resteel`}>
             <main className="mt-16 min-h-screen sm:mt-20">
-                <ContactCTA />
+                <ContactCTA siteSettings={siteSettings} />
             </main>
         </Layout>
     );
