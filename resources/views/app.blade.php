4f55bad2e4485080b59a6ae96531dfed14157e19
shikhar26shukla <shikhar@devloops.in>
2025-08-19 11:15:38 +0530

    favion change

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -19,16 +19,19 @@
     <meta name="description" content="Resteel Solutions B.V. - Preparing sorted materials for recycling, Helmond.">
     <meta property="og:title" content="Resteel Solutions B.V.">
     <meta property="og:description" content="Sustainable solutions for material preparation and recycling.">
-    <meta property="og:image" content="{{ asset('favicon.ico') }}">
+    <meta property="og:image" content="{{ asset('assets/ResteelSolutionsLogo.png') }}">
     <meta property="og:type" content="website">
 
-    <link rel="icon" href="{{ asset('favicon.ico') }}?v=2" sizes="any">
-    <link rel="icon" type="image/png" href="{{ asset('favicon.ico') }}?v=2">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon.ico') }}?v=2">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon.ico') }}?v=2">
-    <link rel="icon" type="image/png" sizes="48x48" href="{{ asset('favicon.ico') }}?v=2">
-    <link rel="apple-touch-icon" href="{{ asset('favicon.ico') }}?v=2">
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}?v=2" type="image/png">
+    <!-- <link rel="icon" href="{{ asset('favicon.ico') }}?v=2" sizes="any"> -->
+    <!-- <link href="{{ asset('public/assets/ResteelSolutionsLogo.png') }}" rel="icon"> -->
+    <link href="{{ asset('assets/ResteelSolutionsLogo.png') }}" rel="icon">
+
+    <!-- <link rel="icon" type="image/png" href="{{ asset('assets/ResteelSolutionsLogo.png') }}?v=2">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/ResteelSolutionsLogo.png') }}?v=2">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/ResteelSolutionsLogo.png') }}?v=2">
+    <link rel="icon" type="image/png" sizes="48x48" href="{{ asset('assets/ResteelSolutionsLogo.png') }}?v=2">
+    <link rel="apple-touch-icon" href="{{ asset('assets/ResteelSolutionsLogo.png') }}?v=2">
+    <link rel="shortcut icon" href="{{ asset('assets/ResteelSolutionsLogo.png') }}?v=2" type="image/png"> -->
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
